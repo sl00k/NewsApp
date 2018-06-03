5a437ffe12e775b9c33f8691e2621276fbdc9b9a
sl00k <morcra@gmx.de>
2018-06-03 15:45:57 +0200

    added settings menu to filter news rubric

--- app/src/main/java/com/example/android/newsapp/MainActivity.java
+++ app/src/main/java/com/example/android/newsapp/MainActivity.java
@@ -5,12 +5,16 @@ import android.app.LoaderManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
@@ -26,9 +30,10 @@ import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<News>> {
     public static final String LOG_TAG = MainActivity.class.getName();
-    private static final String BASE_URL = "https://content.guardianapis.com/search?show-fields=trailText&show-tags=contributor&api-key=";
+    // private static final String BASE_URL = "https://content.guardianapis.com/search?show-fields=trailText&show-tags=contributor&api-key=";
+    private static final String BASE_URL = "https://content.guardianapis.com/search";
     String apiKey = BuildConfig.THE_GUARDIAN_API_KEY;
-    String REQUEST_URL = BASE_URL + apiKey;
+    //String REQUEST_URL = BASE_URL + apiKey;
     //public static final String REQUEST_URL = "https://newsapi.org/v2/top-headlines?country=de&category=business&apiKey=5f99e4501411422a877e52d2cb8aa22e";
     private static final int News_Loader_ID = 1;
     private SwipeRefreshLayout mSwipeRefreshLayout;
@@ -42,14 +47,38 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     @Override
     public Loader<List<News>> onCreateLoader(int i, Bundle bundle) {
 
+        //Rewrite the URL to get the data
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
 
+        // getString retrieves a String value from the preferences. The second parameter is the default value for this preference.
+        String sectionSelected = sharedPrefs.getString(
+                getString(R.string.settings_news_rubric_key),
+                getString(R.string.settings_news_rubric_default));
+
+        // parse breaks apart the URI string that's passed into its parameter
+        Uri baseUri = Uri.parse(BASE_URL);
+
+        // buildUpon prepares the baseUri that we just parsed so we can add query parameters to it
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        // Append query parameter and its value.
+        uriBuilder.appendQueryParameter("show-fields", "trailText");
+        uriBuilder.appendQueryParameter("show-tags", "contributor");
+        uriBuilder.appendQueryParameter("api-key", apiKey);
+        uriBuilder.appendQueryParameter("section", sectionSelected);
+
+        // Return the completed uri `https://content.guardianapis.com/search?show-fields=trailText&show-tags=contributor&api-key=
+        return new NewsLoader(this, uriBuilder.toString());
+
+    }
+
+
+/*
         mEmptyStateTextView = findViewById(R.id.empty_view);
         // falls keine daten vorhanden sind
         ListView newsListView = findViewById(R.id.list);
         newsListView.setEmptyView(mEmptyStateTextView);
-        return new NewsLoader(this, REQUEST_URL);
-
-    }
+        return new NewsLoader(this, REQUEST_URL);*/
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -71,7 +100,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
             // Get details on the currently active default data network
             NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-
+            // mEmptyStateTextView = findViewById(R.id.empty_view);
             // If there is a network connection, fetch data
             if (networkInfo != null && networkInfo.isConnected()) {
 
@@ -80,7 +109,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             } else {
                 // Otherwise, display error
 
-                mEmptyStateTextView = findViewById(R.id.empty_view);
+                // mEmptyStateTextView = findViewById(R.id.empty_view);
                 // Update empty state with no connection error message
                 mEmptyStateTextView.setText(R.string.no_internet_connection);
             }
@@ -124,18 +153,24 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             // First, hide loading indicator so error message will be visible
             View loadingIndicator = findViewById(R.id.progress_bar);
             loadingIndicator.setVisibility(View.GONE);
-            mEmptyStateTextView = findViewById(R.id.empty_view);
+            //mEmptyStateTextView = findViewById(R.id.empty_view);
             // Update empty state with no connection error message
             mEmptyStateTextView.setText(R.string.no_internet_connection);
         }
     }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find a reference to the {@link ListView} in the layout
         ListView newsListView = findViewById(R.id.list);
+        mEmptyStateTextView = findViewById(R.id.empty_view);
+        newsListView.setEmptyView(mEmptyStateTextView);
 
-
+        // check if there is an internet connection
         testConnection();
 
         // Create a new adapter that takes an empty list of news as input
@@ -173,4 +208,26 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         });
 
     }
+
+    // inflate the menu
+    @Override
+    // This method initialize the contents of the Activity's options menu.
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the Options Menu we specified in XML
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    // check which items of the menu is selected
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
+
---
+++ app/src/main/java/com/example/android/newsapp/SettingsActivity.java
@@ -0,0 +1,45 @@
+package com.example.android.newsapp;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.newsapp.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+
+    }
+
+    public static class NewsPreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_main);
+
+            Preference section = findPreference(getString(R.string.settings_news_rubric_key));
+            bindPreferenceSummaryToValue(section);
+        }
+
+        private void bindPreferenceSummaryToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = preferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, preferenceString);
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String stringValue = value.toString();
+            preference.setSummary(stringValue);
+            return true;
+        }
+    }
+}
